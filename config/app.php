c4913671bead10f010ac189c5879d5fff4d8a634
Michael Chelen <michael.chelen@gmail.com>
2016-05-11 10:27:08 -0400

    steps from orm/installation

--- config/app.php
+++ config/app.php
@@ -156,6 +156,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * 3rd Party Service Providers...
+         */
+        LaravelDoctrine\ORM\DoctrineServiceProvider::class,
+
     ],
 
     /*
@@ -201,6 +206,9 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'EntityManager' => LaravelDoctrine\ORM\Facades\EntityManager::class,
+        'Registry'      => LaravelDoctrine\ORM\Facades\Registry::class,
+        'Doctrine'      => LaravelDoctrine\ORM\Facades\Doctrine::class,
 
     ],
 
